f8eb7226fd8de9a4326ade59bd9e92155de2d29a
borsuczyna <libxxerty@gmail.com>
2022-09-22 17:59:48 +0200

    delete unused files

--- compiled/scripts/Render/Template/main.js
+++
@@ -1,8 +0,0 @@
-import { Render } from '../main.js';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
-export async function renderTemplate() {
-    let page = await Render('main', __dirname, null, true);
-    return page;
-}
