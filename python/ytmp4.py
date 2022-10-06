e0176773f9090d83348993d88433a8e473cff9b8
borsuczyna <libxxerty@gmail.com>
2022-10-06 20:16:33 +0200

    Youtube downloader 1.0

--- python/ytmp4.py
+++ python/ytmp4.py
@@ -1,6 +1,6 @@
 from utils import *
 from time import sleep
-from pytube import Youtube
+from pytube import YouTube
 import sys
 
 # Setup variables
@@ -18,7 +18,31 @@ println('[SUCCESS] Process started!')
 
 println('[DEBUG] [Youtube downloader (mp4)] Url: ' + url + ' | quality: ' + quality + ' | output: ' + outputFile)
 
-# Download video
-println('[INFO] Downloading video...')
-video = Youtube('https:/youtube.com/watch?v=' + url)
-print(video)
+def percent(self, tem, total):
+    perc = (float(tem) / float(total)) * float(100)
+    return perc
+
+def progress_function(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    println('[PROGRESS] ' + str(int(percentage_of_completion)))
+
+# Find stream
+println('[INFO] Getting video info...')
+video = YouTube('https://youtu.be/' + url, on_progress_callback=progress_function)
+streams = video.streams.filter(progressive=True, file_extension='mp4')
+stream = streams.filter(res=quality)
+if len(stream) == 0:
+    stream = streams.first()
+
+if not stream:
+    println('[ERROR] No valid sources for this video are available')
+
+# Download
+println('[INFO] Found stream, downloading...')
+stream.download(filename=outputFile, output_path=directory)
+
+# Done
+println('[SUCCESS] Done!')
+println('[FINISH] ' + outputFile)
